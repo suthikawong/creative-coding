388d32ce267be22204011db8564aca7db6f888c6
suthikawong <suthika.wsrd@gmail.com>
2023-03-18 18:47:46 +0700

    animate object

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -4,6 +4,7 @@ const random = require('canvas-sketch-util/random')
 
 const settings = {
     dimensions: [1080, 1080],
+    animate: true,
 }
 
 const sketch = ({ context, width, height }) => {
@@ -14,13 +15,19 @@ const sketch = ({ context, width, height }) => {
         agents.push(new Agent(x, y))
     }
     return ({ context, width, height }) => {
-        agents.forEach((agent) => agent.draw(context))
+        context.fillStyle = 'white'
+        context.fillRect(0, 0, width, height)
+        agents.forEach((agent) => {
+            agent.update()
+            agent.draw(context)
+            agent.bounce(width, height)
+        })
     }
 }
 
 canvasSketch(sketch, settings)
 
-class Point {
+class Vector {
     constructor(x, y) {
         this.x = x
         this.y = y
@@ -29,13 +36,34 @@ class Point {
 
 class Agent {
     constructor(x, y) {
-        this.pos = new Point(x, y)
-        this.radius = 10
+        this.pos = new Vector(x, y)
+        this.vel = new Vector(random.range(-1, 1), random.range(-1, 1))
+        this.radius = random.range(4, 12)
+    }
+
+    bounce(width, height) {
+        if (this.pos.x <= 0 || this.pos.x >= width) {
+            this.vel.x *= -1
+        }
+        if (this.pos.y <= 0 || this.pos.y >= height) {
+            this.vel.y *= -1
+        }
+    }
+
+    update() {
+        this.pos.x += this.vel.x
+        this.pos.y += this.vel.y
     }
     draw(context) {
+        context.save()
+        context.translate(this.pos.x, this.pos.y)
+        context.lineWidth = 4
+
         context.beginPath()
-        context.arc(this.pos.x, this.pos.y, this.radius, 0, math.degToRad(360))
-        context.fillStyle = 'black'
+        context.arc(0, 0, this.radius, 0, math.degToRad(360))
         context.fill()
+        context.stroke()
+
+        context.restore()
     }
 }
