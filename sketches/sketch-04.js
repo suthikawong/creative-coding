da68a4a91913fca1dc99a5e5f8a48626c75d9c77
suthikawong <suthika.wsrd@gmail.com>
2023-03-19 13:40:44 +0700

    add control panel

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -1,16 +1,32 @@
 const canvasSketch = require('canvas-sketch')
+const random = require('canvas-sketch-util/random')
+const math = require('canvas-sketch-util/math')
+const Tweakpane = require('tweakpane')
 
 const settings = {
     dimensions: [1080, 1080],
+    animate: true,
+}
+
+const params = {
+    cols: 10,
+    rows: 10,
+    scaleMin: 1,
+    scaleMax: 30,
+    freq: 0.001,
+    amp: 0.2,
+    animate: true,
+    frame: 0,
+    lineCap: 'butt',
 }
 
 const sketch = () => {
-    return ({ context, width, height }) => {
+    return ({ context, width, height, frame }) => {
         context.fillStyle = 'white'
         context.fillRect(0, 0, width, height)
 
-        const cols = 10
-        const rows = 10
+        const cols = params.cols
+        const rows = params.rows
         const numCells = cols * rows
         const gridw = width * 0.8
         const gridh = height * 0.8
@@ -28,14 +44,32 @@ const sketch = () => {
             const w = cellw * 0.8
             const h = cellh * 0.8
 
+            const f = params.animate ? frame : params.frame
+
+            // คืนค่า -1 ถึง 1
+            // frequency ทำให้การหมุนองศาเปลี่ยนแปลง
+            // frame ทำให้เกิด animation
+            // const n = random.noise2D(x + frame * 10, y, params.freq)
+            const n = random.noise3D(x, y, f * 10, params.freq)
+            const angle = n * Math.PI * params.amp // จะได้มุม -180 degrees ถึง 180 degrees
+            const scale = math.mapRange(
+                n,
+                -1,
+                1,
+                params.scaleMin,
+                params.scaleMax
+            )
+
             context.save()
             // translate 3 ครั้งนี้เท่ากับเอามาบวกกันแล้ว translate ทีเดียว
             context.translate(x, y)
             context.translate(margx, margy)
             context.translate(cellw * 0.5, cellh * 0.5)
             // context.translate(x + margx + cellw * 0.5, y + margy + cellh * 0.5)
+            context.rotate(angle)
 
-            context.lineWidth = 4
+            context.lineWidth = scale
+            context.lineCap = params.lineCap
 
             context.beginPath()
             context.moveTo(w * -0.5, 0)
@@ -46,4 +80,25 @@ const sketch = () => {
     }
 }
 
+const createPane = () => {
+    const pane = new Tweakpane.Pane()
+    let folder
+
+    folder = pane.addFolder({ title: 'Grid' })
+    folder.addInput(params, 'lineCap', {
+        options: { butt: 'butt', round: 'round', square: 'square' },
+    })
+    folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 })
+    folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 })
+    folder.addInput(params, 'scaleMin', { min: 1, max: 100 })
+    folder.addInput(params, 'scaleMax', { min: 1, max: 100 })
+
+    folder = pane.addFolder({ title: 'Noise' })
+    folder.addInput(params, 'freq', { min: -0.01, max: 0.01 })
+    folder.addInput(params, 'amp', { min: 0, max: 1 })
+    folder.addInput(params, 'animate')
+    folder.addInput(params, 'frame', { min: 0, max: 999 })
+}
+
+createPane()
 canvasSketch(sketch, settings)
