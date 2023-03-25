3327cbbfc3b9653d139ec64d4669b1fe301c2d92
suthikawong <suthika.wsrd@gmail.com>
2023-03-25 23:31:22 +0700

    draw skewed rectangle

--- sketches/sketch-06.js
+++ sketches/sketch-06.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch')
+const math = require('canvas-sketch-util/math')
 
 const settings = {
     dimensions: [1080, 1080],
@@ -6,6 +7,7 @@ const settings = {
 
 const sketch = () => {
     let x, y, w, h
+    let angle, rx, ry
     return ({ context, width, height }) => {
         context.fillStyle = 'white'
         context.fillRect(0, 0, width, height)
@@ -17,20 +19,30 @@ const sketch = () => {
         context.save()
         context.translate(x, y)
         context.strokeStyle = 'blue'
-        context.strokeRect(w * -0.5, h * -0.5, w, h)
-
-        // อีกวิธีที่ใช้ในการวาดสี่เหลี่ยม
-        // context.translate(w * -0.5, h * -0.5)
-        // context.beginPath()
-        // context.moveTo(0, 0)
-        // context.lineTo(w, 0)
-        // context.lineTo(w, h)
-        // context.lineTo(0, h)
-        // context.closePath()
-        // context.stroke()
+        drawSkewedRect({ context })
 
         context.restore()
     }
+}
+
+const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
+    const angle = math.degToRad(degrees)
+
+    const rx = Math.cos(angle) * w
+    const ry = Math.sin(angle) * w
+
+    context.save()
+
+    context.translate(rx * -0.5, (ry + h) * -0.5)
+    context.beginPath()
+    context.moveTo(0, 0)
+    context.lineTo(rx, ry)
+    context.lineTo(rx, ry + h)
+    context.lineTo(0, h)
+    context.closePath()
+    context.stroke()
+
+    context.restore()
 }
 
 canvasSketch(sketch, settings)
