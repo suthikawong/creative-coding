5aa0c2c9da77618c95fc25c6674399f7796dee94
suthikawong <suthika.wsrd@gmail.com>
2023-04-02 19:47:20 +0700

    interaction with curve

--- sketches/sketch-curve.js
+++ sketches/sketch-curve.js
@@ -2,14 +2,22 @@ const canvasSketch = require('canvas-sketch')
 
 const settings = {
     dimensions: [1080, 1080],
+    animate: true,
 }
 
-const sketch = () => {
-    const points = [
+let elCanvas
+let points
+
+const sketch = ({ canvas }) => {
+    points = [
         new Point({ x: 200, y: 540 }),
         new Point({ x: 400, y: 300, control: true }),
         new Point({ x: 800, y: 540 }),
     ]
+
+    canvas.addEventListener('mousedown', onMouseDown)
+    elCanvas = canvas
+
     return ({ context, width, height }) => {
         context.fillStyle = 'white'
         context.fillRect(0, 0, width, height)
@@ -30,6 +38,37 @@ const sketch = () => {
     }
 }
 
+const onMouseDown = (e) => {
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+
+    const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width
+    const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height
+
+    points.forEach((point) => {
+        point.isDragging = point.hitTest(x, y)
+    })
+}
+
+const onMouseMove = (e) => {
+    // ค่าที่ได้จาก offsetX เป็นค่า pixel ที่มีการ scale เพื่อให้สามารถแสดง canvas ทั้งหมดบน browser ได้ไม่ว่ารูปจะใหญ่แค่ไหน
+    // เลยต้องมีการคำนวนหาค่า x ในหน่วย pixel ที่ค่ามากสุดเป็นความกว้างของ canvas (ปรับ scale)
+    const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width
+    const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height
+
+    points.forEach((point) => {
+        if (point.isDragging) {
+            point.x = x
+            point.y = y
+        }
+    })
+}
+
+const onMouseUp = () => {
+    window.removeEventListener('mousemove', onMouseMove)
+    window.removeEventListener('mouseup', onMouseUp)
+}
+
 class Point {
     constructor({ x, y, control = false }) {
         this.x = x
@@ -45,6 +84,16 @@ class Point {
         context.arc(0, 0, 10, 0, 2 * Math.PI)
         context.fill()
         context.restore()
+    }
+
+    // เช็คว่าจุดที่ mousedown เป็นจุดที่ plot บน canvas หรือไม่
+    hitTest(x, y) {
+        const dx = this.x - x
+        const dy = this.y - y
+        const dd = Math.sqrt(dx * dx + dy * dy)
+        // ถ้าระยะห่างระหว่างจุดที่ plot และจุดที่ mousedown ไม่เกิน 20 แสดงว่ากำลังลากจุดนั้นอยู่
+        // (รัศมีของจุดคือ 10)
+        return dd < 20
     }
 }
 
