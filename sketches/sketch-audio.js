cc714d6c4a54033f68306ea5562829c0d157b318
suthikawong <suthika.wsrd@gmail.com>
2023-04-06 16:59:13 +0700

    add audio to browser

---
+++ sketches/sketch-audio.js
@@ -0,0 +1,30 @@
+const canvasSketch = require('canvas-sketch')
+
+const settings = {
+    dimensions: [1080, 1080],
+}
+
+let audio
+
+const sketch = () => {
+    audio = document.createElement('audio')
+    audio.src = '../audio/Sculpture.mp3'
+    audio.volume = 0.3
+    return ({ context, width, height }) => {
+        context.fillStyle = 'white'
+        context.fillRect(0, 0, width, height)
+    }
+}
+
+const addListeners = () => {
+    window.addEventListener('mouseup', () => {
+        if (audio.paused) {
+            audio.play()
+        } else {
+            audio.pause()
+        }
+    })
+}
+
+addListeners()
+canvasSketch(sketch, settings)
