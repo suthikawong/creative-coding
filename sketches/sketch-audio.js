55fd33ea7047bdb7215b26b343c37c7c6be3e530
suthikawong <suthika.wsrd@gmail.com>
2023-04-06 17:26:02 +0700

    get audio frequency and display

--- sketches/sketch-audio.js
+++ sketches/sketch-audio.js
@@ -2,29 +2,73 @@ const canvasSketch = require('canvas-sketch')
 
 const settings = {
     dimensions: [1080, 1080],
+    animate: true,
 }
 
 let audio
+let audioContext, audioData, sourceNode, analyserNode
+let manager
 
 const sketch = () => {
-    audio = document.createElement('audio')
-    audio.src = '../audio/Sculpture.mp3'
-    audio.volume = 0.3
     return ({ context, width, height }) => {
         context.fillStyle = 'white'
         context.fillRect(0, 0, width, height)
+        if (!analyserNode) return
+
+        analyserNode.getFloatFrequencyData(audioData)
+        const avg = getAverage(audioData)
+        context.save()
+        context.translate(width * 0.5, height * 0.5)
+        context.lineWidth = 10
+        context.beginPath()
+        context.arc(0, 0, Math.abs(avg), 0, Math.PI * 2)
+        context.stroke()
+        context.restore()
     }
 }
 
 const addListeners = () => {
     window.addEventListener('mouseup', () => {
+        if (!audioContext) {
+            createAudio()
+        }
         if (audio.paused) {
             audio.play()
+            manager.play()
         } else {
             audio.pause()
+            manager.pause()
         }
     })
 }
 
-addListeners()
-canvasSketch(sketch, settings)
+const createAudio = () => {
+    audio = document.createElement('audio')
+    audio.src = '../audio/Sculpture.mp3'
+    audio.volume = 0.3
+    audioContext = new AudioContext()
+
+    sourceNode = audioContext.createMediaElementSource(audio)
+    sourceNode.connect(audioContext.destination)
+
+    analyserNode = audioContext.createAnalyser()
+    sourceNode.connect(analyserNode)
+
+    audioData = new Float32Array(analyserNode.frequencyBinCount)
+}
+
+const getAverage = (data) => {
+    let sum = 0
+    for (let i = 0; i < data.length; i++) {
+        sum += data[i]
+    }
+    return sum / data.length
+}
+
+const start = async () => {
+    addListeners()
+    manager = await canvasSketch(sketch, settings)
+    manager.pause()
+}
+
+start()
